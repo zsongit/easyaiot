28cd6789494c744b75fd4f41f4f0a6a1b2bf2409
翱翔的雄库鲁 <andywebjava@163.com>
2025-11-19 11:12:04 +0800

    udp(WEB):修复PhsyicalModal组件的路径问题

---
+++ WEB/src/views/product/components/Detail.vue
@@ -0,0 +1,33 @@
+<template>
+  <BasicDrawer @register="register" title="产品详情" width="60%">
+    <Tabs v-model:activeKey="activeKey">
+      <TabPane v-for="item in tabPaneList" :key="item.key" :tab="item.label">
+        <component :is="item.componentName" />
+      </TabPane>
+
+      <!-- <template #tabBarExtraContent>
+        <a-button>Extra Action</a-button>
+      </template> -->
+    </Tabs>
+  </BasicDrawer>
+</template>
+
+<script lang="ts" setup name="Detail">
+  import { BasicDrawer, useDrawerInner } from '@/components/Drawer';
+  import { Tabs, TabPane } from 'ant-design-vue';
+  import { ref } from 'vue';
+  import PhysicalModal from './PhysicalModal.vue';
+
+  const [register, { setDrawerProps, closeDrawer }] = useDrawerInner(() => {});
+
+  const activeKey = ref('PhysicalModal');
+  const tabPaneList = [
+    {
+      label: '物理模型管理',
+      key: 'PhysicalModal',
+      componentName: PhysicalModal,
+    },
+  ];
+</script>
+
+<style lang="less" scoped></style>
